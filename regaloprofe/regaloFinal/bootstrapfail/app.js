3849241c69800bb43b02ef387554b36f6d77abe0
Laura-2950 <mlaurafernandez84@gmail.com>
2021-09-07 23:31:56 -0300

    Actualización 07-09

---
+++ regaloprofe/regaloFinal/bootstrapfail/app.js
@@ -0,0 +1,41 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+
+document.querySelector('.container').addEventListener('mousemove', function (eyeball) {
+    var eye = document.querySelectorAll('.eye');
+    eye.forEach(function (eye) {
+        let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 4);
+        let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 4);
+        let radian = Math.atan2(eyeball.pageX - x, eyeball.pageY - y);
+        let rot = (radian * (180 / Math.PI) * -1) + 270;
+        eye.style.transform = "rotate(" + rot + "deg)";
+    })
+})
